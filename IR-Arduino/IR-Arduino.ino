e19e2e3e6ea3ef382b5b43306d56cf15f4c63a11
enra64 <sujomear@arcor.de>
2017-08-25 12:58:45 +0200

    added final test case

--- IR-Arduino/IR-Arduino.ino
+++ IR-Arduino/IR-Arduino.ino
@@ -1,20 +1,20 @@
-/// This is the actual implementation that should run on the thermometer
-
-#include "AdvancedThermometerLogic.hpp"
-
 #define RUN 0
 #define TEST_INPUT 1
 #define TEST_DISPLAY 2
 #define TEST_LOGGER 3
 #define TEST_THERMOMETER_WRAPPER 4
-#define MODE TEST_LOGGER
+#define MODE TEST_DISPLAY
 
 
 #if MODE == RUN
+/// This is the actual implementation that should run on the thermometer
+#include "AdvancedThermometerLogic.hpp"
+
 AdvancedThermometerLogic *thermometerLogic;
 Input *inputHandler;
 
 void setup() {
+    Serial.begin(115200);
     inputHandler = new Input(
             shortTriggerListener,
             longTriggerListener,
@@ -37,9 +37,11 @@ void loop() {
 }
 
 #elif MODE == TEST_INPUT
+#include "Input.hpp"
 Input *inputHandler;
 
 void setup() {
+    Serial.begin(115200);
     inputHandler = new Input(
             shortTriggerListener,
             longTriggerListener,
@@ -73,53 +75,102 @@ void loop() {
     inputHandler->update();
 }
 #elif MODE == TEST_DISPLAY
+
+#include "Display.hpp"
+
+Display* display;
+float example_data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+float* example_data_full_res;
+
+void setup(){
+    display = new Display();
+
+    Serial.begin(115200);
+    Serial.print("graph horizontal resolution: ");
+    Serial.println(display->getGraphHorizontalResolution());
+
+    example_data_full_res = new float[display->getGraphHorizontalResolution()];
+    for(int i = 0; i < display->getGraphHorizontalResolution(); i++){
+        example_data_full_res[i] = i * i;
+    }
+}
+
+void loop() {
+    display->update(example_data, 11, true, true, true);
+    delay(1000);
+    display->update(example_data, 11, false, false, false);
+    delay(1000);
+}
+
 #elif MODE == TEST_LOGGER
 
-    #include <Logger.hpp>
+#include <Logger.hpp>
 
-void printBuffer(float* buffer, size_t length) {
-    for(int i = 0; i < length; i++)
+void printBuffer(float *buffer, size_t length) {
+    for (int i = 0; i < length; i++)
         Serial.println(buffer[i]);
     Serial.println("finished buffer printing");
 }
 
-    void setup() {
-        Logger::init();
+void setup() {
+    Logger::init();
+    Serial.begin(115200);
 
-        Serial.println("appending 1.1, 2.2, 3.3, 4.5 to log");
-        Logger::append(1.1);
-        Logger::append(2.2);
-        Logger::append(3.3);
-        Logger::append(4.5);
+    Serial.println("appending 1.1, 2.2, 3.3, 4.5 to log");
 
-        Serial.print("retrieving last value: ");
-        float lastValue;
-        Logger::getLastEntry(lastValue);
-        Serial.println(lastValue);
+    long time_logger = millis();
+    Logger::append(1.1);
+    Serial.print("appending once took (ms): ");
+    Serial.println(millis() - time_logger);
 
-        float buffer[10];
+    Logger::append(2.2);
+    Logger::append(3.3);
+    Logger::append(4.5);
 
-        Serial.println("retrieving less than full log:");
-        for(int i = 0; i < 10; i++) buffer[i] = 0;
-        Logger::getLog(buffer, 2);
-        printBuffer(buffer, 10);
+    Serial.print("retrieving last value: ");
+    float lastValue;
+    Logger::getLastEntry(lastValue);
+    Serial.println(lastValue);
 
-        Serial.println("retrieving exactly the full log:");
-        for(int i = 0; i < 10; i++) buffer[i] = 0;
-        Logger::getLog(buffer, 4);
-        printBuffer(buffer, 10);
+    float buffer[10];
 
-        Serial.println("retrieving more than full log:");
-        for(int i = 0; i < 10; i++) buffer[i] = 0;
-        Logger::getLog(buffer, 12);
-        printBuffer(buffer, 10);
-    }
+    Serial.println("retrieving less than full log:");
+    for (int i = 0; i < 10; i++) buffer[i] = 0;
+    Logger::getLog(buffer, 2);
+    printBuffer(buffer, 10);
 
-    void loop() {
+    Serial.println("retrieving exactly the full log:");
+    for (int i = 0; i < 10; i++) buffer[i] = 0;
+    Logger::getLog(buffer, 4);
+    printBuffer(buffer, 10);
 
-    }
+    Serial.println("retrieving more than full log:");
+    for (int i = 0; i < 10; i++) buffer[i] = 0;
+    Logger::getLog(buffer, 12);
+    printBuffer(buffer, 10);
+}
+
+void loop() {
+
+}
 
 #elif MODE == TEST_THERMOMETER_WRAPPER
+
+#include "ThermometerWrapper.h"
+
+Thermometer *thermometer;
+
+void setup() {
+    thermometer = new Thermometer();
+    Serial.begin(115200);
+}
+
+void loop() {
+    Serial.print("temperatur: ");
+    Serial.println(thermometer->getTemperature());
+    delay(1000);
+}
+
 #else
 #error "INVALID OPERATION MODE"
 #endif
